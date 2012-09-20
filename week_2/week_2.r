086b630163dc71c19a3c77f77478f367691c1d57
Peter Darche <pdarche@gmail.com>
2012-09-20 18:55:16 -0400

    assignment finished, but changes need to be made

--- week_2/week_2.r
+++ week_2/week_2.r
@@ -10,7 +10,7 @@ snf <- read.csv(file="http://www.jakeporway.com/teaching/data/snf_2.csv", head=T
 #total stops
 total.stopps <- nrow(snf)
 total.frisks <- sum(snf$frisked)
-frisks.by.race <- rev(sort(table(snf$race[snf$frisked == '1'])))
+frisks.by.race <- rev(sort(table(snf$race[snf$frisked == 1])))
 
 # Which race leads to the highest percentage of frisks?
 most.frisked.race <- frisks.by.race[1]
@@ -38,18 +38,17 @@ barplot(top.50, xlab="crime suspected", ylab="number of stops", col="red", densi
 # 	suspected crimes and look at those.  If we were to just look at stops where the 
 # 	crime.suspected was one of the top 30 crimes, what percentage of the stops would that 
 # 	cover?  Do you think that’s enough? 
+#A: [1] 0.9132194
 
 sum.top.30 <- sum(sorted.crimes[1:30])
 perc.top.30 <- sum.top.30/total.stops
-#A: [1] 0.9132194
-
 
 #Write code to create a variable called “crime.abbv” that consists of just the 
 # first three letters of crime.suspected and show the code to add it to our main data frame.  
-# Now what percentage of the stops do the top 30 crime.abbvs account for?
+# Now what percentage of the stops do the top 30 crime.abbv account for?
 
-crime.abbvs <- substr(snf$crime.suspected, 1, 3)
-sorted.crimes.abbvs <- rev(sort(table(crime.abbvs)))
+crime.abbv <- substr(snf$crime.suspected, 1, 3)
+sorted.crimes.abbvs <- rev(sort(table(crime.abbv)))
 top.30.abbvs <- sorted.crimes.abbvs[1:30]
 
 # Write code to show the top 3 crimes each race is suspected of (rev(), 
@@ -58,7 +57,7 @@ top.30.abbvs <- sorted.crimes.abbvs[1:30]
 # different.  What are these differences?  
 
 for(i in 1:6){
-  print(rev(sort(table(crime.abbvs[snf$race == i]))))
+  print(rev(sort(table(crime.abbv[snf$race == i]))))
 }
 
 # 1 : FEL  MIS CPW  
@@ -67,7 +66,17 @@ for(i in 1:6){
 # 4 : FEL  MIS  BUR
 # 5 : FEL ROB MIS
 # 6 : FEL ROB GLA
+
+#I also tried this:
+top.3 <- function(...){
+rev(sort(table(crime.abbv[snf$race == ... ])))[1:3]
+}
+
+top.three.by.race <- tapply(sort(crime.abbv), snf$race, top.3)
 	
+# however, I couldn't figure out how r was incrementing through the top 3 function and was given errors.  
+# use a number but would be returned 6 tables with the same data 	 
+
 # PART 2 
 
 # Let’s create an “hour” variable that tells us what hour of the day each stop
@@ -76,8 +85,8 @@ for(i in 1:6){
 # the substr() function we learned about above to strip out the hour, then use 
 # as.numeric() from lecture 2 to convert it to a number.
 
-time.of.stop <- substr(snf$time, 12, 13)
-time.of.stop.sorted <- rev(sort(table(as.numeric(hour))))
+hour <- substr(snf$time, 12, 13)
+time.of.stop <- rev(sort(table(as.numeric(hour))))
 
 # 20   21   19   22   18   16   15   17   23   14    1    0   13    2   12   11   10    3    9    4    8    5    7    6 
 # 4607 4158 4141 3721 3387 3319 3228 3220 3209 3128 3057 3022 2490 2213 2181 2071 1572 1378 1218  905  740  477  324  323 
@@ -86,12 +95,35 @@ time.of.stop.sorted <- rev(sort(table(as.numeric(hour))))
 # day has the most stops?  Which hour has the fewest?
 
 time.of.stop <- (table(as.numeric(hour)))
-plot(as.vector(time.of.stop), type="l", xlab="time of stop", ylab="number of stops")
+color.vector <- rep(1,24)
+color.vector[6] <- 2
+color.vector[20] <- 3
+plot(as.vector(time.of.stop), type="l", xlab="time of stop", ylab="number of stops", col=color.vector)
 
 # Create the same plot but with points instead of lines.  Use a different plotting 
 # symbol than the default and color the max point and min points different colors
+plot(as.vector(time.of.stop), type="o", xlab="time of stop", ylab="number of stops", col=color.vector)
+
+# BONUS
+# weight
+fixed.values <- snf[snf$weight > 40 & snf$weight < 400 & snf$height > 40,]
+weight <- fixed.values$weight
+height <- fixed.values$height
+bmi <- (weight)*703/(height*height)
 
 
-#weight
-good.weight <- snf[snf$weight > 40 & snf$weight < 400,]
-# (weight)*703/(height*height)
+# Q: The US Government defines people with BMIs 30 or higher to be obese.  What 
+#    percentage of people with BMI’s greater than or equal to 30 who were stopped 
+#    were ultimately arrested?
+
+bmi.over.30 <- with.bmi[with.bmi$bmi > 30, ]
+num.over.30 <- nrow(bmi.over.30)
+over.30.arrested <- bmi.over.30[bmi.over.30$arrested == 1]
+percent.over.30.arrested <- over.30.arrested/num.over.30
+
+
+bmi.under.30 <- with.bmi[with.bmi$bmi < 30, ]
+num.under.30 <- nrow(bmi.under.30)
+under.30.arrested <- bmi.under.30[bmi.under.30$arrested == 1]
+percent.under.30.arrested <- under.30.arrested/num.under.30
+
