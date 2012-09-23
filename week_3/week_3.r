5ab12d57d40044b76bac7ead6dfcc8ebfaceb788
Peter Darche <pdarche@gmail.com>
2012-09-23 17:50:36 -0400

    created data files and modified python script to include retweet count

---
+++ week_3/week_3.r
@@ -0,0 +1,10 @@
+# PART 1
+
+# • I’m always curious about people who have high follower counts, because they 
+# might be interesting people tweeting about this topic.  How many unique users 
+# have more than 100,000 followers?  What are their screen names?
+# • It’d be interesting to see what part of the world users are tweeting from.  What 
+# are the top 3 locations people are from (not counting blanks)?
+# • Retweets can often indicate what’s important, or at least influential.  What is the 
+# text of the tweet that was retweeted the most times and who tweeted it?
+
